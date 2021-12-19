c596797dc4a65be1de54b7a7f581d0db5c15c833
QINGDKJ <qingdkj@outlook.com>
2021-12-19 18:10:53 +0800

    Add fast-read

---
+++ fast-read.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+
+
+inline int/* the 'int' can  replace to __int128 etc.*/ read() {
+  int x = 0, f = 1;
+  char ch = getchar();
+  while (ch < '0' || ch > '9') {
+    if (ch == '-')
+      f = -1;
+    ch = getchar();
+  }
+  while (ch >= '0' && ch <= '9') {
+    x = (x << 1) + (x << 3) + (ch ^ 48);
+    ch = getchar();
+  }
+  return x * f;
+}
+
+// int128 writing
+inline void write(__int128 x) {
+  if (x < 0) {
+    putchar('-');
+    x = -x;
+  }
+  if (x > 9)
+    write(x / 10);
+  putchar(x % 10 + '0');
+}
